077ef9740ce05b275506d85b17558c0d15b32cff
Sajid Ansari <sajidansari33272@gmail.com>
2020-11-11 23:49:06 +0545

    Class based component changed to funtion

--- src/Components/Layouts/Layouts.js
+++ src/Components/Layouts/Layouts.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home/Home";
 import About from "./About/About";
@@ -9,33 +9,24 @@ import Sidebar from "./Navigation/Sidebar";
 import Auxillary from "../../hoc/Auxillary";
 import Pagenotfound from "./PageNotFound/PageNotFound";
 
-class Layouts extends Component {
-  state = {
-    isopensidebar: false,
+const Layouts = () => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const Opensidebar = () => {
+    setIsSideBarOpen(!isSideBarOpen);
   };
-  Opensidebar = () => {
-    this.setState((preState) => ({
-      isopensidebar: !preState.isopensidebar,
-    }));
-  };
-  render() {
-    return (
-      <Auxillary>
-        <Sidebar
-          Opensidebar={this.state.isopensidebar}
-          clicked={this.Opensidebar}
-        />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/resume" component={Resume} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact component={Pagenotfound} />
-        </Switch>
-      </Auxillary>
-    );
-  }
-}
+  return (
+    <Auxillary>
+      <Sidebar Opensidebar={isSideBarOpen} clicked={Opensidebar} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact component={Pagenotfound} />
+      </Switch>
+    </Auxillary>
+  );
+};
 
 export default Layouts;
